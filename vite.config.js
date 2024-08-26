ca1a045356ee83ed8ae3ec8e27a7303c403bb785
vinoth <vinothdeveloper12@gmail.com>
2024-08-26 06:30:24 +0530

    first time

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/social-media/",
   build: {
     outDir: "dist", // Ensure this is set to 'dist'
   },
